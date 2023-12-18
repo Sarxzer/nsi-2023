d2a7699bc4a5adbce7a5255f672ee231986f4899
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2023-12-18 09:38:35 +0100

    Add files via upload

--- Python/Chaine de caractere/exo/6.py
+++ Python/Chaine de caractere/exo/6.py
@@ -1,8 +1,8 @@
 def caesar_crypt(text:str,rot:int):
     letter = "abcdefghijklmnopqrstuvwxyz"
     out = ""
-    for i in range(1, len(text)+1):
-        out += letter[i+rot-(26*int((i+rot)/26))-1]
+    for i in range(0, len(text)):
+        out += (letter[letter.index(text[i])+rot-26*(int((i+rot)/26))] if text[i].islower() else letter[letter.index(text[i].lower())+rot-26*(int((i+rot)/26))].upper()) if text[i].lower() in letter else text[i]
     return out
 
 print(caesar_crypt(input("text: "),int(input("rot: "))))
